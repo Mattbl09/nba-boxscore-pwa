985bbccb702a39ea237058a33f881f060ee01c20
NAFTA\u877729 (Matt Black) <matthew.black@syngenta.com>
2020-06-06 16:57:12 -0400

    Unable to map through array of data
    Unable to map through array of data

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import './App.css';
 
 const App = () => {
     const [date, setDate] = useState('');
-    const [games, setGames] = useState({});
+    const [games, setGames] = useState([]);
 
     const search = async (e) => {
         if(e.key === 'Enter') {
             const data = await fetchGames(date)
 
-            setGames(data);
             console.log(data);
+            setGames(data);
             setDate('');
         }
     }
@@ -21,7 +21,30 @@ const App = () => {
         <div className = "main-container">
             <input
                 type="text" className="search" placeholder="Search Date..." value={date} onChange={(e) => setDate(e.target.value)} onKeyPress={search}/>
+                {games.data && ([games.data].map(game => (
+                <div key={game.id} className="score"> 
+                    <h2 className="score-name">
+                        <span>{game.date}</span>
+                        <sup>{game.home_team}</sup>                            
+                    </h2>
+                </div>
+                )))}
         </div>
+
+        /*
+        <div className = "main-container">
+            <input
+                type="text" className="search" placeholder="Search Date..." value={date} onChange={(e) => setDate(e.target.value)} onKeyPress={search}/>
+                {games.data && (
+                    <div className="score"> 
+                        <h2 className="score-name">
+                            <span key={games.id}>{games.data.home_team}</span>
+                            <sup key={games.id}>{games.data.home_team}</sup>                            
+                        </h2>
+                    </div>
+                )}
+        </div>
+        */
     )
 }
 
