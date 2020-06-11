864e01737811bf3e6e9fe5ff3d820c7fdc0d9e5c
NAFTA\u877729 (Matt Black) <matthew.black@syngenta.com>
2020-06-11 16:40:44 -0400

    Retrieved boxscore data

--- src/App.js
+++ src/App.js
@@ -1,128 +1,165 @@
-import React, {useState} from 'react';
-import {fetchGames} from './api/fetchData';
-import './App.css';
+import React, { useState } from "react";
+import { fetchGames, fetchBoxscore } from "./api/fetchData";
+import "./App.css";
 
 const App = () => {
-    const [date, setDate] = useState('');
-    const [games, setGames] = useState({});
+  const [date, setDate] = useState("");
+  const [gameID, setGameID] = useState({});
+  const [games, setGames] = useState({});
 
-    const [showScore, setShowscore] = useState(true);
-    
-    const search = async (e) => {
-        if(e.key === 'Enter') {
-            const data = await fetchGames(date)
+  const [showScore, setShowscore] = useState(true);
 
-            console.log(data);
-            setGames(data);
-        }
+  const searchDate = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchGames(date);
+
+      console.log(data);
+      setGames(data);
     }
+  };
 
-    return (
-        <div className = "main-container">
+  const boxscoreData = async (e) => {
+    const data = await fetchBoxscore(gameID);
 
-                { showScore && <input type="date" className="search" placeholder="Search Date..." value={date} onChange={(e) => setDate(e.target.value)} onKeyPress={search}></input> }
-                {games.data && showScore && (games.data.map(game => (
-                <div key={game.id} className="score">
-                    <table>
-                        <tbody> 
-                            <tr className="score-name">
-                                <td style={{textAlign: 'right'}}>{game.home_team.full_name}:</td>
-                                <td>{game.home_team_score}</td>
-                                <td rowSpan="2"><button className="button" onClick={() => setShowscore(!showScore)}>boxscore</button></td>
-                                <td></td>
-                            </tr>
-                            <tr className="score-name"> 
-                                <td style={{textAlign: 'right'}}>{game.visitor_team.full_name}:</td>
-                                <td>{game.visitor_team_score}</td>                            
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                )))}
-                {!showScore && <div>
-                    <table className="boxscore-table">
-                        <thead className="boxscore-table-headers">
-                            <tr>
-                                <th colSpan="14" >Home Team</th>
-                            </tr>
-                        </thead>
-                        <thead>
-                            <tr>
-                                <th>Player Name</th>
-                                <th>MIN</th>
-                                <th>PTS</th>
-                                <th>FG</th>
-                                <th>3PT</th>
-                                <th>FT</th>
-                                <th>OREB</th>
-                                <th>DREB</th>
-                                <th>REB</th>
-                                <th>AST</th>
-                                <th>STL</th>
-                                <th>BLK</th>
-                                <th>TO</th>
-                                <th>PF</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    Test
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <table className="boxscore-table">
-                        <thead className="boxscore-table-headers">
-                            <tr>
-                                <th colSpan="14" >Visitor Team</th>
-                            </tr>
-                        </thead>
-                        <thead>
-                            <tr>
-                                <th>Player Name</th>
-                                <th>MIN</th>
-                                <th>PTS</th>
-                                <th>FG</th>
-                                <th>3PT</th>
-                                <th>FT</th>
-                                <th>OREB</th>
-                                <th>DREB</th>
-                                <th>REB</th>
-                                <th>AST</th>
-                                <th>STL</th>
-                                <th>BLK</th>
-                                <th>TO</th>
-                                <th>PF</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    Test
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>}
+    console.log(data);
+    setGameID(data);
+  };
+
+  /*
+  const sorted = gameID.data.sort((a, b) => {
+    return b.pts - a.pts;
+  });
+  */
+
+  return (
+    <div className="main-container">
+      {showScore && (
+        <input
+          type="date"
+          className="search"
+          placeholder="Search Date..."
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+          onKeyPress={searchDate}
+        ></input>
+      )}
+      {games.data &&
+        showScore &&
+        games.data.map((game) => (
+          <div key={game.id} className="score">
+            <table>
+              <tbody>
+                <tr className="score-name">
+                  <td style={{ textAlign: "right" }}>
+                    {game.home_team.full_name}:
+                  </td>
+                  <td>{game.home_team_score}</td>
+                  <td rowSpan="2">
+                    <button
+                      className="button"
+                      onClick={(e) => {
+                        setShowscore(!showScore);
+                        boxscoreData(game.id);
+                      }}
+                    >
+                      boxscore
+                    </button>
+                  </td>
+                  <td></td>
+                </tr>
+                <tr className="score-name">
+                  <td style={{ textAlign: "right" }}>
+                    {game.visitor_team.full_name}:
+                  </td>
+                  <td>{game.visitor_team_score}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        ))}
+      {!showScore && gameID.data && (
+        <div>
+          <table className="boxscore-table">
+            <thead className="boxscore-table-headers">
+              <tr>
+                <th colSpan="14">Home Team</th>
+              </tr>
+            </thead>
+            <thead>
+              <tr>
+                <th>Player Name</th>
+                <th>MIN</th>
+                <th>PTS</th>
+                <th>FG</th>
+                <th>3PT</th>
+                <th>FT</th>
+                <th>OREB</th>
+                <th>DREB</th>
+                <th>REB</th>
+                <th>AST</th>
+                <th>STL</th>
+                <th>BLK</th>
+                <th>TO</th>
+                <th>PF</th>
+              </tr>
+              {gameID.data
+                .sort(
+                  ({ pts: previousPTS }, { pts: currentPTS }) =>
+                    currentPTS - previousPTS
+                )
+                .map((gameID) => (
+                  <tr key={gameID.id}>
+                    <th>
+                      {gameID.player.first_name} {gameID.player.last_name}
+                    </th>
+                    <th>{gameID.min}</th>
+                    <th>{gameID.pts}</th>
+                  </tr>
+                ))}
+            </thead>
+            <tbody>
+              <tr>
+                <td>Test</td>
+              </tr>
+            </tbody>
+          </table>
+          <table className="boxscore-table">
+            <thead className="boxscore-table-headers">
+              <tr>
+                <th colSpan="14">Visitor Team</th>
+              </tr>
+            </thead>
+            <thead>
+              <tr>
+                <th>Player Name</th>
+                <th>MIN</th>
+                <th>PTS</th>
+                <th>FG</th>
+                <th>3PT</th>
+                <th>FT</th>
+                <th>OREB</th>
+                <th>DREB</th>
+                <th>REB</th>
+                <th>AST</th>
+                <th>STL</th>
+                <th>BLK</th>
+                <th>TO</th>
+                <th>PF</th>
+              </tr>
+              {gameID.data.map((gameID) => (
+                <tr key={gameID.id}></tr>
+              ))}
+            </thead>
+            <tbody>
+              <tr>
+                <td>Test</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-
-        /*
-        <div className = "main-container">
-            <input
-                type="text" className="search" placeholder="Search Date..." value={date} onChange={(e) => setDate(e.target.value)} onKeyPress={search}/>
-                {games.data && (
-                    <div className="score"> 
-                        <h2 className="score-name">
-                            <span key={games.id}>{games.data.home_team}</span>
-                            <sup key={games.id}>{games.data.home_team}</sup>                            
-                        </h2>
-                    </div>
-                )}
-        </div>
-        */
-    )
-}
-
+      )}
+    </div>
+  );
+};
 
 export default App;
--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -1,13 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
 
 //const gamesURL = `https://www.balldontlie.io/api/v1/games/?dates[]=`;
 
 export const fetchGames = async (date) => {
-    const {data} = await axios.get('https://www.balldontlie.io/api/v1/games/', {
-        params: {
-            dates: [date],
-        }
-    });
+  const { data } = await axios.get("https://www.balldontlie.io/api/v1/games/", {
+    params: {
+      dates: [date],
+    },
+  });
 
-    return data;
-}
+  return data;
+};
+
+export const fetchBoxscore = async (gameID) => {
+  const { data } = await axios.get("https://www.balldontlie.io/api/v1/stats/", {
+    params: {
+      game_ids: gameID,
+    },
+  });
+
+  return data;
+};
