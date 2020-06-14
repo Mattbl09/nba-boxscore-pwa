be26210b877109c6c14888b9a32ec31687193cd3
NAFTA\u877729 <matthew.black@syngenta.com>
2020-06-14 15:57:38 -0400

    Adding Service Worker and Offline

--- src/App.js
+++ src/App.js
@@ -28,6 +28,10 @@ const App = () => {
 
   return (
     <div className="main-container">
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+      ></meta>
       {showScore && (
         <input
           type="date"
